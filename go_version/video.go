c92fa7c6a510e08f36652867e23b351a6d542945
拜晨 <oncwnuHPS2TzIWyoiXvfZ0aEaw1o@git.weixin.qq.com>
2019-07-23 20:54:27 +0800

    add ResizeVideo function

--- go_version/video.go
+++ go_version/video.go
@@ -13,8 +13,45 @@ func CreateImgByBGR(sizex int, sizey int, b float64, g float64, r float64) gocv.
 	return img
 }
 
+// ResizeVideo resize a video, sizeX/Y refer to width/height
+func ResizeVideo(srcPath string, dstPath string, sizeX, sizeY int) (err error) {
+	capt, err := gocv.VideoCaptureFile(srcPath)
+	defer capt.Close()
+	if err != nil {
+		return
+	}
+
+	fps := capt.Get(gocv.VideoCaptureFPS)
+	writer, err := gocv.VideoWriterFile(dstPath, "avc1", fps, sizeX, sizeY, true)
+	defer writer.Close()
+	if err != nil {
+		return
+	}
+
+	frame := gocv.NewMat()
+	dstFrame := gocv.NewMat()
+	defer frame.Close()
+	defer dstFrame.Close()
+	for {
+		if ok := capt.Read(&frame); ok {
+			gocv.Resize(frame, &dstFrame, image.Pt(sizeX, sizeY), 0, 0, gocv.InterpolationLinear)
+			//win.IMShow(dstFrame)
+			//gocv.WaitKey(100)
+			err = writer.Write(dstFrame)
+			if err != nil {
+				return
+			}
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
+
+
 // Convert replace green screen with a custom color
-func Convert(srcPath string, dstPath string, r, g, b float64) {
+func Convert(srcPath string, dstPath string, r, g, b float64) (err error) {
 	lb := gocv.NewScalar(68, 84, 153, 255)
 	ub := gocv.NewScalar(80, 255, 255, 255)
 
@@ -35,7 +72,7 @@ func Convert(srcPath string, dstPath string, r, g, b float64) {
 	capt, err := gocv.VideoCaptureFile(srcPath)
 	defer capt.Close()
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	fps := capt.Get(gocv.VideoCaptureFPS)
@@ -45,7 +82,6 @@ func Convert(srcPath string, dstPath string, r, g, b float64) {
 	defer writer.Close()
 
 	if err != nil {
-		panic(err)
 		return
 	}
 
@@ -75,11 +111,15 @@ func Convert(srcPath string, dstPath string, r, g, b float64) {
 			break
 		}
 	}
+	return nil
 }
 
 
 
-//func main() {
-//	Convert("video.mp4", "output.mp4", 230, 230, 230)
-//}
+func main() {
+	err := ResizeVideo("video.mp4", "resize.mp4", 960, 720)
+	if err != nil {
+		panic(err)
+	}
+}
 
